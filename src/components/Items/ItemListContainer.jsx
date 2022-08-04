71806879684d56b235b532d3640182152dd61023
Fabian Tureo Avalith <fabianestebantureo@gmail.com>
2022-08-03 21:48:42 -0300

    :bug: Fix: resolve bugs in dependencies to correct rendering. Clean up for warnings in console

--- src/components/Items/ItemListContainer.jsx
+++ src/components/Items/ItemListContainer.jsx
@@ -1,79 +1,101 @@
-import React, { useEffect, useState }  from 'react'
-import ItemList from './ItemList'
+import React, { useEffect, useState } from "react";
+import ItemList from "./ItemList";
 
-import LoadingSymbol from '../LoadingSymbol';
-import { useParams, Link } from 'react-router-dom';
+import LoadingSymbol from "../LoadingSymbol";
+import { useParams, Link } from "react-router-dom";
 
-const ItemListContainer = () => { 
-  const [allProducts, setAllProducsts] = useState([])
-  const [productsArray, setProductsArray] = useState([])
-  const {category} = useParams()
-  
-  const [loading, setLoading] = useState(true)
-  const loadingSymbol = <LoadingSymbol/>
+const ItemListContainer = () => {
+    const [allProducts, setAllProducsts] = useState([]);
+    const [productsArray, setProductsArray] = useState([]);
+    const { category } = useParams();
 
-  useEffect(()=>{
-    fetch("../../JSON/DataList.json")
-    .then((res)=>res.json())
-    .then((products)=>setAllProducsts(products))
-    .finally(()=> setLoading(false))
-    
-    console.log(allProducts)
+    const [loading, setLoading] = useState(true);
+    const loadingSymbol = <LoadingSymbol />;
 
-    getProducts()
-    setClickedCategory()
-  }, [category])
+    useEffect(() => {
+        fetch("../../JSON/DataList.json")
+            .then((res) => res.json())
+            .then((products) => setAllProducsts(products))
+            .finally(() => setLoading(false));
 
-  // categoría
-  const getProducts = () => {
-    let search = document.querySelector("#search-text").value.toLowerCase().trim()
-    const productsSearched = allProducts.filter((product)=>product.name.toLowerCase().includes(search.toLowerCase()))
+        console.log(allProducts);
 
-    if (category){
-      const productsFiltered = productsSearched.filter((product)=>product.category == category)
-      setProductsArray(productsFiltered)
-    }
+        // getProducts()
+        // setClickedCategory();
+    }, [category, productsArray, allProducts]);
 
-    else {
-      setProductsArray(productsSearched)
-    }
-  }
-  
-  // botones de categoría (css)
-  const setClickedCategory = ()=>{
-    document.querySelectorAll("button").forEach((button)=>{
-      button.classList.remove("clicked")
-    })
+    useEffect(() => {
+        getProducts();
+        setClickedCategory();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [productsArray]);
 
-    if (document.querySelector(`.${category}`)){
-      document.querySelector(`.${category}`).classList.add("clicked")
-    }
+    // // categoría
+    const getProducts = () => {
+        let search = document
+            .querySelector("#search-text")
+            .value.toLowerCase()
+            .trim();
+        const productsSearched = allProducts.filter((product) =>
+            product.name.toLowerCase().includes(search.toLowerCase())
+        );
 
-    else {
-      document.querySelector(".todo").classList.add("clicked")
-    }
-  }
+        if (category) {
+            const productsFiltered = productsSearched.filter(
+                (product) => product.category === category
+            );
+            setProductsArray(productsFiltered);
+        } else {
+            setProductsArray(productsSearched);
+        }
+    };
 
-  return (
-    <div className="products">
-      <div className='categoryFilter'>
-        <div className="search">
-          <input type="search" placeholder='Busca acá el nombre del producto...' id='search-text' onChange={()=>getProducts()}/>
-          <button >Buscar</button>
+    // botones de categoría (css)
+    const setClickedCategory = () => {
+        document.querySelectorAll("button").forEach((button) => {
+            button.classList.remove("clicked");
+        });
+
+        if (document.querySelector(`.${category}`)) {
+            document.querySelector(`.${category}`).classList.add("clicked");
+        } else {
+            document.querySelector(".todo").classList.add("clicked");
+        }
+    };
+
+    return (
+        <div className="products">
+            <div className="categoryFilter">
+                <div className="search">
+                    <input
+                        type="search"
+                        placeholder="Busca acá el nombre del producto..."
+                        id="search-text"
+                        onChange={() => getProducts()}
+                    />
+                    <button>Buscar</button>
+                </div>
+                <div className="filter">
+                    <Link to="/products">
+                        <button className="todo">Todo</button>
+                    </Link>
+                    <Link to="/products/cápsula">
+                        <button className="cápsula">Cápsula</button>
+                    </Link>
+                    <Link to="/products/elementos">
+                        <button className="elementos">Elementos</button>
+                    </Link>
+                    <Link to="/products/café-molido">
+                        <button className="café-molido">Café molido</button>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="itemListContainer">
+                {loading ? loadingSymbol : <ItemList items={productsArray} />}
+            </div>
         </div>
-        <div className="filter">
-          <Link to="/products"><button className="todo" >Todo</button></Link>
-          <Link to="/products/cápsula"><button className="cápsula">Cápsula</button></Link>
-          <Link to="/products/elementos"><button className="elementos" >Elementos</button></Link>
-          <Link to="/products/café-molido"><button className="café-molido" >Café molido</button></Link>
-        </div>
-      </div>
+    );
+};
 
-      <div className='itemListContainer'>
-        { loading ? loadingSymbol : <ItemList items={productsArray}/>}
-      </div>
-    </div>
-  )
-}
-
-export default ItemListContainer
+export default ItemListContainer;
